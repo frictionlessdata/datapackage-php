2177aac47184eb1d2d72acb7907d714a9c163405
Daniel Pritchard <daniel@pritchard.co>
2018-01-03 13:10:42 +1300

    More helpful exceptions when JSON is invalid

--- src/Factory.php
+++ src/Factory.php
@@ -312,6 +312,11 @@ class Factory
                     }
                 }
             }
+            if(json_last_error()){
+				throw new Exceptions\DatapackageInvalidSourceException(
+					json_last_error_msg().' when loading source: '.json_encode($source)
+				);
+            }
         } else {
             throw new Exceptions\DatapackageInvalidSourceException(
                 'Invalid source: '.json_encode($source)
